28bea10eca8ffffe82435749b45641667060ffec
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-10-03 10:39:08 -0300

    fix: Add @Repository annotation to StreamingRepository

--- src/main/java/com/movieflix/repository/StreamingRepository.java
+++ src/main/java/com/movieflix/repository/StreamingRepository.java
@@ -2,7 +2,8 @@ package com.movieflix.repository;
 
 import com.movieflix.entity.Streaming;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StreamingRepository extends JpaRepository<Streaming, Long> {
-
 }
