769373a9875010a1c4a472c36af55491c7f8311f
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-10-05 10:46:59 -0300

    chore(movie): Add initial structure for Movie module (controller, service, repository)

---
+++ src/main/java/com/movieflix/service/MovieService.java
@@ -0,0 +1,13 @@
+package com.movieflix.service;
+
+import com.movieflix.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MovieService {
+
+    private final MovieRepository repository;
+
+}
