a394d49bd689b6333fad223736821ed17bc4e098
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-10-20 22:26:43 -0300

    feat(auth): handle invalid username or password with custom exception and controller advice

---
+++ src/main/java/com/movieflix/config/ApplicationControllerAdvice.java
@@ -0,0 +1,17 @@
+package com.movieflix.config;
+
+import com.movieflix.exception.UsernameOrPasswordInvalidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(UsernameOrPasswordInvalidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String HandleNotFoundException(UsernameOrPasswordInvalidException ex) {
+        return ex.getMessage();
+    }
+}
