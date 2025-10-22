f14c336a9cbf69a144249d0e75d02d2cba4edc2b
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-10-22 10:48:57 -0300

    docs(movie): add Swagger documentation to request and response DTOs

--- src/main/java/com/movieflix/controller/response/MovieResponse.java
+++ src/main/java/com/movieflix/controller/response/MovieResponse.java
@@ -1,19 +1,27 @@
 package com.movieflix.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Builder
-public record MovieResponse(Long id,
+public record MovieResponse(@Schema(type = "long", description = "Código do filme")
+                            Long id,
+                            @Schema(type = "string", description = "Nome do filme")
                             String title,
+                            @Schema(type = "string", description = "Descrição do filme")
                             String description,
+                            @Schema(type = "date", description = "Data de lançamento do filme. Ex: '19/09/1999'")
                             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                             LocalDate releaseDate,
+                            @Schema(type = "double", description = "Score do filme. Ex: 7.8")
                             double rating,
+                            @Schema(type = "array", description = "Lista de códigos de categoria")
                             List<CategoryResponse> categories,
+                            @Schema(type = "array", description = "Lista de códigos de serviço de streaming")
                             List<StreamingResponse> streamings
 ) {
 }
